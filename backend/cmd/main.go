8b1879f2f5e57386a27188a6556cb2e99bba263e
staceyng <stacey.ng@motional.com>
2024-04-28 16:04:35 -0600

    feat: add barebones frontend with GetRecipeByName API

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -7,11 +7,30 @@ import (
 )
 
 func main() {
-	r := gin.Default()
+	r := gin.New()
+	r.Use(CORSMiddleware())
+
 	r.GET("/ping", api.PingPong)
 	r.GET("/recipes", api.GetRecipe)
+
 	err := r.Run()
 	if err != nil {
 		panic(err)
+	}
+}
+
+func CORSMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "OPTIONS, GET")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+
+		c.Next()
 	}
 }
---
+++ frontend/public/script.js
@@ -0,0 +1,39 @@
+const form = document.querySelector("form");
+const searchResultsDiv = document.querySelector("#search-results");
+
+form.addEventListener("submit", async (e) => {
+  console.log("fire submited");
+  e.preventDefault();
+  let name = document.querySelector("#name").value;
+  name = name.replace(" ", "-");
+
+  let headers = new Headers();
+  headers.append("Content-Type", "application/json");
+  headers.append("Accept", "application/json");
+  headers.append("Access-Control-Allow-Origin", "http://localhost:8080");
+  headers.append("Access-Control-Allow-Credentials", "true");
+  headers.append("GET", "OPTIONS");
+
+  const response = await fetch(
+    `http://localhost:8080/recipes?name=${name}`,
+    headers
+  );
+  const result = await response.json();
+  console.log(result);
+
+  // Update the HTML to display the results
+  searchResultsDiv.innerHTML = "";
+  const recipe = result.data;
+  console.log(recipe);
+  const recipeHTML = `
+      <h3>${recipe.name}</h3>
+      <p>Category: ${recipe.category}</p>
+      <p>Ingredients:</p>
+        <ul>
+          ${recipe.ingredients
+            .map((ingredient) => `<li>${ingredient}</li>`)
+            .join("")}
+        </ul>
+    `;
+  searchResultsDiv.innerHTML += recipeHTML;
+});
