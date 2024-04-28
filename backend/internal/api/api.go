956dfcb7f002bcfe2228dd5771d0483c0bc7297c
staceyng <stacey.ng@motional.com>
2024-04-28 00:40:15 -0600

    feat: add api to query by category

--- backend/internal/api/api.go
+++ backend/internal/api/api.go
@@ -21,9 +21,23 @@ func PingPong(c *gin.Context) {
 
 func GetRecipe(c *gin.Context) {
 	// GET /recipes?name=<recipe_name>
-	queryName := c.Query("name")
-	recipeName := strings.ReplaceAll(queryName, "-", " ")
+	// GET /recipes?category=<category_name>
 
+	recipeQueryName := c.Query("name")
+	categoryQueryName := c.Query("category")
+
+	if len(recipeQueryName) > 0 {
+		getRecipeByRecipeName(c, recipeQueryName)
+	}
+
+	if len(categoryQueryName) > 0 {
+		getRecipesByCategory(c, categoryQueryName)
+	}
+
+}
+
+func getRecipeByRecipeName(c *gin.Context, s string) {
+	recipeName := strings.ReplaceAll(s, "-", " ")
 	recipeIngredientList := repo.GetRecipeByName(recipeName)
 
 	var ingredients []string
@@ -31,20 +45,56 @@ func GetRecipe(c *gin.Context) {
 	var recipe Recipe
 	caser := cases.Title(language.English)
 
-	if len(recipeIngredientList) > 0 {
-		for _, v := range recipeIngredientList {
-			ingredients = append(ingredients, caser.String(v.IngredientName))
-			cat = v.Category
-		}
-		recipe = Recipe{
-			Name:        caser.String(recipeName),
-			Category:    caser.String(cat),
-			Ingredients: ingredients,
-		}
-		c.JSON(http.StatusOK, JsonResponse{Data: recipe})
-	} else {
-		e := ErrorResponse{Code: "recipe_not_found", Title: "Recipe Not Found", Detail: fmt.Sprintf("Unable to find recipe with name '%s'", recipeName)}
+	if len(recipeIngredientList) == 0 {
+		e := ErrorResponse{Code: "recipe_not_found", Title: "Recipe Not Found", Detail: fmt.Sprintf("Unable to find recipe with name '%s'", s)}
 		errors := []ErrorResponse{e}
 		c.JSON(http.StatusNotFound, JsonResponse{Data: nil, Errors: errors})
 	}
+
+	for _, v := range recipeIngredientList {
+		ingredients = append(ingredients, caser.String(v.IngredientName))
+		cat = v.Category
+	}
+	recipe = Recipe{
+		Name:        caser.String(recipeName),
+		Category:    caser.String(cat),
+		Ingredients: ingredients,
+	}
+	c.JSON(http.StatusOK, JsonResponse{Data: recipe})
+}
+
+func getRecipesByCategory(c *gin.Context, s string) {
+	cat := strings.ReplaceAll(s, "-", " ")
+	recipeIngredientList := repo.GetRecipesByCategory(cat)
+
+	if len(recipeIngredientList) == 0 {
+		e := ErrorResponse{Code: "category_not_found", Title: "Category Not Found", Detail: fmt.Sprintf("Unable to find recipes under category '%s'", cat)}
+		errors := []ErrorResponse{e}
+		c.JSON(http.StatusNotFound, JsonResponse{Data: nil, Errors: errors})
+	}
+
+	recipesMap := make(map[string]*Recipe)
+
+	for _, row := range recipeIngredientList {
+		name := row.RecipeName
+		ingredient := row.IngredientName
+
+		recipe, ok := recipesMap[name]
+		if !ok {
+			recipe = &Recipe{
+				Name:        name,
+				Category:    cat,
+				Ingredients: []string{ingredient},
+			}
+			recipesMap[name] = recipe
+		}
+		recipe.Ingredients = append(recipe.Ingredients, ingredient)
+	}
+
+	var recipes []Recipe
+	for _, recipe := range recipesMap {
+		recipes = append(recipes, *recipe)
+	}
+
+	c.JSON(http.StatusOK, JsonResponse{Data: recipes})
 }
--- backend/internal/repo/repo.go
+++ backend/internal/repo/repo.go
@@ -69,3 +69,52 @@ func GetRecipeByName(name string) []RecipeIngredient {
 	log.Printf("[repo] GetRecipeByName: %v", riList)
 	return riList
 }
+
+func GetRecipesByCategory(category string) []RecipeIngredient {
+	connStr := fmt.Sprintf("host=%s port=%d user=%s password='' dbname=%s sslmode=disable", host, port, user, dbname)
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	// q := "SELECT * FROM categories"
+	q := `
+	SELECT
+		r.name AS recipe_name,
+		r.category AS category,
+		i.name AS ingredient_name
+	FROM
+		recipes r
+	INNER JOIN
+		recipe_ingredients ri ON r.recipe_id = ri.recipe_id
+	INNER JOIN
+		ingredients i ON ri.ingredient_id = i.ingredient_id
+	WHERE
+		r.category = $1
+	`
+
+	rows, err := db.Query(q, category)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer rows.Close()
+
+	var riList = []RecipeIngredient{}
+	for rows.Next() {
+		var rn, c, in string
+		err := rows.Scan(&rn, &c, &in)
+		if err != nil {
+			log.Fatal(err)
+		}
+		ri := RecipeIngredient{
+			RecipeName:     rn,
+			Category:       c,
+			IngredientName: in,
+		}
+		riList = append(riList, ri)
+	}
+
+	log.Printf("[repo] GetRecipesByCategory: %v", riList)
+	return riList
+}
